470634bc15de1532ab255555ff71adf112fd52e8
WangRunji <wangrunji0408@163.com>
2019-04-03 12:49:33 +0800

    add userland example

---
+++ examples/user/src/main.rs
@@ -0,0 +1,83 @@
+use std::{boxed::Box, sync::Arc};
+
+use rcore_thread::{context::Registers, std_thread as thread, *};
+
+const STACK_SIZE: usize = 0x2000;
+const MAX_CPU_NUM: usize = 1;
+const MAX_PROC_NUM: usize = 32;
+
+fn main() {
+    // init processor
+    let scheduler = scheduler::RRScheduler::new(5);
+    let thread_pool = Arc::new(ThreadPool::new(scheduler, MAX_PROC_NUM));
+    unsafe {
+        processor().init(0, Thread::init(), thread_pool);
+    }
+    // init threads
+    thread::spawn(|| {
+        let tid = processor().tid();
+        println!("[{}] yield", tid);
+        thread::yield_now();
+        println!("[{}] spawn", tid);
+        let t2 = thread::spawn(|| {
+            let tid = processor().tid();
+            println!("[{}] yield", tid);
+            thread::yield_now();
+            println!("[{}] return 8", tid);
+            8
+        });
+        println!("[{}] join", tid);
+        let ret = t2.join();
+        println!("[{}] get {:?}", tid, ret);
+        println!("[{}] exit", tid);
+    });
+    // run threads
+    processor().run();
+}
+
+#[repr(C)]
+struct Thread {
+    rsp: *mut Registers,
+    stack: [u8; STACK_SIZE],
+}
+
+impl Thread {
+    unsafe fn init() -> Box<Self> {
+        Box::new(core::mem::uninitialized())
+    }
+    fn new(entry: extern "C" fn(usize) -> !, arg0: usize) -> Box<Self> {
+        let mut thread = unsafe { Thread::init() };
+        let stack_top = thread.stack.as_ptr() as usize + STACK_SIZE;
+        thread.rsp = unsafe { Registers::new(entry, arg0, stack_top) };
+        thread
+    }
+}
+
+/// Implement `switch_to` for a thread
+impl Context for Thread {
+    /// Switch to another thread.
+    unsafe fn switch_to(&mut self, target: &mut Context) {
+        let (to, _): (&mut Thread, usize) = core::mem::transmute(target);
+        Registers::switch(&mut self.rsp, &mut to.rsp);
+    }
+}
+
+/// Define global `Processor` for each core.
+static PROCESSORS: [Processor; MAX_CPU_NUM] = [Processor::new()];
+
+/// Now we only have one core.
+fn cpu_id() -> usize {
+    0
+}
+
+/// Implement dependency for `rcore_thread::std_thread`
+#[no_mangle]
+pub fn processor() -> &'static Processor {
+    &PROCESSORS[cpu_id()]
+}
+
+/// Implement dependency for `rcore_thread::std_thread`
+#[no_mangle]
+pub fn new_kernel_context(entry: extern "C" fn(usize) -> !, arg0: usize) -> Box<Context> {
+    Thread::new(entry, arg0)
+}
--- src/interrupt.rs
+++ src/interrupt.rs
@@ -1,63 +1,90 @@
 //! Enable and disable interrupt for each architecture.
 
-#[inline(always)]
-#[cfg(target_arch = "x86_64")]
-pub unsafe fn disable_and_store() -> usize {
-    let rflags: usize;
-    asm!("pushfq; popq $0; cli" : "=r"(rflags) ::: "volatile");
-    rflags & (1 << 9)
-}
+#[cfg(all(not(feature = "userland"), target_arch = "x86_64"))]
+pub use self::x86_64::*;
 
-#[inline(always)]
+#[cfg(all(not(feature = "userland"), any(target_arch = "riscv32", target_arch = "riscv64")))]
+pub use self::riscv::*;
+
+#[cfg(all(not(feature = "userland"), target_arch = "aarch64"))]
+pub use self::aarch64::*;
+
+#[cfg(feature = "userland")]
+pub use self::dummy::*;
+
 #[cfg(target_arch = "x86_64")]
-pub unsafe fn restore(flags: usize) {
-    if flags != 0 {
-        asm!("sti" :::: "volatile");
+mod x86_64 {
+    #[inline]
+    pub unsafe fn disable_and_store() -> usize {
+        let rflags: usize;
+        asm!("pushfq; popq $0; cli" : "=r"(rflags) ::: "volatile");
+        rflags & (1 << 9)
+    }
+
+    #[inline]
+    pub unsafe fn restore(flags: usize) {
+        if flags != 0 {
+            asm!("sti" :::: "volatile");
+        }
+    }
+
+    #[inline]
+    pub unsafe fn enable_and_wfi() {
+        asm!("sti; hlt" :::: "volatile");
     }
 }
 
-#[inline(always)]
-#[cfg(target_arch = "x86_64")]
-pub unsafe fn enable_and_wfi() {
-    asm!("sti; hlt" :::: "volatile");
-}
-
-#[inline(always)]
 #[cfg(any(target_arch = "riscv32", target_arch = "riscv64"))]
-pub unsafe fn disable_and_store() -> usize {
-    let sstatus: usize;
-    asm!("csrci sstatus, 1 << 1" : "=r"(sstatus) ::: "volatile");
-    sstatus & (1 << 1)
+mod riscv {
+    #[inline]
+    pub unsafe fn disable_and_store() -> usize {
+        let sstatus: usize;
+        asm!("csrci sstatus, 1 << 1" : "=r"(sstatus) ::: "volatile");
+        sstatus & (1 << 1)
+    }
+
+    #[inline]
+    pub unsafe fn restore(flags: usize) {
+        asm!("csrs sstatus, $0" :: "r"(flags) :: "volatile");
+    }
+
+    #[inline]
+    pub unsafe fn enable_and_wfi() {
+        asm!("csrsi sstatus, 1 << 1; wfi" :::: "volatile");
+    }
+
 }
 
-#[inline(always)]
-#[cfg(any(target_arch = "riscv32", target_arch = "riscv64"))]
-pub unsafe fn restore(flags: usize) {
-    asm!("csrs sstatus, $0" :: "r"(flags) :: "volatile");
-}
-
-#[inline(always)]
-#[cfg(any(target_arch = "riscv32", target_arch = "riscv64"))]
-pub unsafe fn enable_and_wfi() {
-    asm!("csrsi sstatus, 1 << 1; wfi" :::: "volatile");
-}
-
-#[inline(always)]
 #[cfg(target_arch = "aarch64")]
-pub unsafe fn disable_and_store() -> usize {
-    let daif: u32;
-    asm!("mrs $0, DAIF; msr daifset, #2": "=r"(daif) ::: "volatile");
-    daif as usize
+mod aarch64 {
+    #[inline]
+    pub unsafe fn disable_and_store() -> usize {
+        let daif: u32;
+        asm!("mrs $0, DAIF; msr daifset, #2": "=r"(daif) ::: "volatile");
+        daif as usize
+    }
+
+    #[inline]
+    pub unsafe fn restore(flags: usize) {
+        asm!("msr DAIF, $0" :: "r"(flags as u32) :: "volatile");
+    }
+
+    #[inline]
+    pub unsafe fn enable_and_wfi() {
+        asm!("msr daifclr, #2; wfi" :::: "volatile");
+    }
 }
 
-#[inline(always)]
-#[cfg(target_arch = "aarch64")]
-pub unsafe fn restore(flags: usize) {
-    asm!("msr DAIF, $0" :: "r"(flags as u32) :: "volatile");
-}
+#[cfg(feature = "userland")]
+mod dummy {
+    #[inline]
+    pub unsafe fn disable_and_store() -> usize {
+        0
+    }
 
-#[inline(always)]
-#[cfg(target_arch = "aarch64")]
-pub unsafe fn enable_and_wfi() {
-    asm!("msr daifclr, #2; wfi" :::: "volatile");
+    #[inline]
+    pub unsafe fn restore(_flags: usize) {}
+
+    #[inline]
+    pub unsafe fn enable_and_wfi() {}
 }
